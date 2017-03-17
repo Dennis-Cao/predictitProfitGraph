ec406cbcc03daa7bbd3b78ea4e50eb5c2ad7ef10
Dennis Cao <DennisCao@live.com>
2017-03-16 18:42:17 -0700

    No longer stores csv in memory

--- graphtool.py
+++ graphtool.py
@@ -5,26 +5,29 @@ import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
 datePoints = []
 profitPoints = []
+cumulativeProfit = []
 totalProfit = 0.0
 
-for line in reversed(list(open("Trade_History.csv"))):
-	try:
-		strippedLine = line.split(',')
-		datePoints.append(dt.datetime.strptime(strippedLine[0].split()[0],'%m/%d/%Y').date())
-		profit = strippedLine[5]
-		if(profit[0] == "("):
-			totalProfit -= float(profit[2:-1])
-		else:
-			totalProfit += float(profit[1:])
-		profitPoints.append(totalProfit)
-	except:
-		pass
+for line in open("Trade_History.csv"):
+    try:
+        strippedLine = line.split(',')
+        datePoints.append(dt.datetime.strptime(strippedLine[0].split()[0],'%m/%d/%Y').date())
+        profit = strippedLine[5]
+        if(profit[0] == "("):
+            profitPoints.append(float(profit[2:-1])*(-1))
+        else:
+            profitPoints.append(float(profit[1:]))
+    except:
+        pass
 
 datePoints.reverse()
 profitPoints.reverse()
+for item in profitPoints:
+    totalProfit += item
+    cumulativeProfit.append(totalProfit)
 fig = plt.figure()           
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
 plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
-plt.plot(datePoints,profitPoints)
+plt.plot(datePoints,cumulativeProfit)
 plt.gcf().autofmt_xdate()
 plt.show()
